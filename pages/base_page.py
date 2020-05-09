c8c193d348a5ce7ce0619d4b4d53c532c1c911d9
ru.supreme@gmail.com <g1thub_m4k3l0v3n0tw4r>
2020-05-09 21:17:11 +0300

    Improve structure of classes and methods, add new test for step 4.3.8

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,13 +1,17 @@
-from selenium.common.exceptions import NoAlertPresentException, NoSuchElementException, TimeoutException
+from .locators import BasePageLocators
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-import math
 
 
 class BasePage:
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
 
     def is_disappeared(self, how, what, timeout=4) -> bool:
         try:
@@ -34,16 +38,5 @@ class BasePage:
     def open(self):
         self.browser.get(self.url)
 
-    def solve_quiz_and_get_code(self):
-        alert = self.browser.switch_to.alert
-        x = alert.text.split(" ")[2]
-        answer = str(math.log(abs((12 * math.sin(float(x))))))
-        alert.send_keys(answer)
-        alert.accept()
-        try:
-            alert = self.browser.switch_to.alert
-            alert_text = alert.text
-            print(f"Your code: {alert_text}")
-            alert.accept()
-        except NoAlertPresentException:
-            print("No second alert presented")
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,5 @@
 from .pages.product_page import ProductPage
-from time import sleep
+from .pages.login_page import LoginPage
 import pytest
 
 
@@ -27,19 +27,19 @@ def test_guest_can_add_product_to_basket(browser, promo):
     product_page.should_be_info_alert(product_page.color_price())
 
 
-def test_guest_cant_see_success_message(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    product_page.should_not_be_success_message()
-
-
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_to_basket()
+    product_page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
     product_page.should_not_be_success_message()
 
 
@@ -50,3 +50,20 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     product_page.open()
     product_page.add_to_basket()
     product_page.should_be_disappeared_success_message()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.should_be_login_link()
+    product_page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
